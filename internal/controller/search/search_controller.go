c2f36ee2a442179ae1798ff05acac6353bc26bc9
Lee Shinhee <shlee@idb.ai>
2025-02-23 14:47:53 +0900

    [ADD] go 루틴을 사용한 다중 페이지 처리 추가

--- internal/controller/search/search_controller.go
+++ internal/controller/search/search_controller.go
@@ -7,52 +7,50 @@ import (
 	service "github.com/shlee2227/go_crawling/internal/service/search"
 )
 
-
 type Controller interface {
-    RegisterRoutes(r *gin.Engine)
+	RegisterRoutes(r *gin.Engine)
 	searchAndStore(ctx *gin.Context)
 	getItems(ctx *gin.Context)
 }
 
-
 type controller struct {
-    service service.Service
+	service service.Service
 }
 
 func NewNaverController(service service.Service) Controller {
-    return &controller{
-        service: service,
-    }
+	return &controller{
+		service: service,
+	}
 }
 
 // Gin 라우터 등록
 func (c *controller) RegisterRoutes(r *gin.Engine) {
-    r.GET("/search", c.searchAndStore)
-    r.GET("/items", c.getItems)
+	r.GET("/search", c.searchAndStore)
+	r.GET("/items", c.getItems)
 }
 
 func (c *controller) searchAndStore(ctx *gin.Context) {
-    searchWord := ctx.Query("search_word")
-    if searchWord == "" {
-        ctx.JSON(http.StatusBadRequest, gin.H{"error": "검색어를 입력해주세요"})
-        return
-    }
+	searchWord := ctx.Query("search_word")
+	if searchWord == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "검색어를 입력해주세요"})
+		return
+	}
 
-    err := c.service.SearchAndStoreItems(searchWord)
-    if err != nil {
-        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	err := c.service.SearchAndStoreItems(searchWord)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    ctx.JSON(http.StatusCreated, gin.H{"message": "검색 및 저장 성공"})
+	ctx.JSON(http.StatusCreated, gin.H{"message": "검색 및 저장 성공"})
 }
 
 func (c *controller) getItems(ctx *gin.Context) {
-    items, err := c.service.GetAllItems()
-    if err != nil {
-        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	items, err := c.service.GetAllItems()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    ctx.JSON(http.StatusOK, items)
+	ctx.JSON(http.StatusOK, items)
 }
--- internal/service/search/search_service.go
+++ internal/service/search/search_service.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
 
 	entities "github.com/shlee2227/go_crawling/internal/entities/search"
 	repository "github.com/shlee2227/go_crawling/internal/repository/search"
@@ -23,7 +24,7 @@ type service struct {
 	repo repository.Repository
 }
 
-// 객체 생성자 
+// 객체 생성자
 func NewService(repo repository.Repository) Service {
 	return &service{
 		repo: repo,
@@ -47,52 +48,88 @@ func (s *service) SearchAndStoreItems(searchText string) error {
 	}
 }
 
-
 // crawlNaver 네이버 api를 통해 검색
 func (s *service) SearchNaver(searchText string) ([]entities.Item, error) {
-	encodedSearchText := url.QueryEscape(searchText)
-	url := fmt.Sprintf("https://openapi.naver.com/v1/search/blog.json?query=%sdisplay=10&start=1&sort=sim", encodedSearchText) // 이 부분을 고루틴으로 여러페이지 한번에 해보기
+	var wg sync.WaitGroup
 
-	// 검색어 request 생성
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Println(err)
-		return nil, fmt.Errorf("naver API 생성 실패")
+	errorchan := make(chan error)
+	resultchan := make(chan []entities.Item)
+
+	for i := 1; i <= 100; i += 10 {
+		wg.Add(1)
+		go func(start int) {
+			defer wg.Done()
+			encodedSearchText := url.QueryEscape(searchText)
+			url := fmt.Sprintf("https://openapi.naver.com/v1/search/blog.json?query=%s&display=10&start=%d&sort=sim", encodedSearchText, start)
+
+			// 검색어 request 생성
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				log.Println(err)
+				errorchan <- fmt.Errorf("naver API 생성 실패: %v", err)
+				return
+			}
+
+			req.Header.Add("X-Naver-Client-Id", os.Getenv("NAVER_CLIENT_ID"))
+			req.Header.Add("X-Naver-Client-Secret", os.Getenv("NAVER_CLIENT_SECRET"))
+
+			// request
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Println(err)
+				errorchan <- fmt.Errorf("naver API 요청 실패: %v", err)
+				return
+			}
+			defer resp.Body.Close() // 응답 본문 닫아 줘야 함
+
+			if resp.StatusCode != http.StatusOK {
+				log.Printf("StatusCode: %d", resp.StatusCode)
+				errorchan <- fmt.Errorf("naver API 요청 실패: %d", resp.StatusCode)
+				return
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+				errorchan <- fmt.Errorf("naver API 응답 읽기 실패: %v", err)
+				return
+			}
+
+			var response entities.NaverResponse
+			err = json.Unmarshal(body, &response)
+			if err != nil {
+				log.Println(err)
+				errorchan <- fmt.Errorf("naver API 응답 parsing 실패: %v", err)
+				return
+			}
+
+			resultchan <- response.Items
+		}(i)
 	}
-
-	req.Header.Add("X-Naver-Client-Id", os.Getenv("NAVER_CLIENT_ID"))
-	req.Header.Add("X-Naver-Client-Secret", os.Getenv("NAVER_CLIENT_SECRET"))
-
-	// request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("StatusCode: %d", resp.StatusCode)
-		return nil, fmt.Errorf("naver API 요청 실패: %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		return nil, fmt.Errorf("naver API 응답 읽기 실패: %d", resp.StatusCode)
-	}
-
-	var response entities.NaverResponse
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		log.Println(err)
-		return nil, fmt.Errorf("naver API 응답 parsing 실패: %d", resp.StatusCode)
-	}
-
 	// for _, item := range response.Items {
-	// 	fmt.Printf("%+v\n", item)
+	// 	fmt.Printf("%+v\n", item) //구조체 필드와 값 출력
 	// 	fmt.Println()
 	// }
-	return response.Items, nil
+
+	go func() {
+		wg.Wait()
+		close(resultchan)
+		close(errorchan)
+	}()
+
+	select {
+	case err := <-errorchan:
+		fmt.Println("에러가 발생하였습니다", err)
+		return nil, err
+	default: //에러 없으면 그냥 진행
+	}
+
+	var result []entities.Item
+	for rs := range resultchan {
+		result = append(result, rs...)
+	}
+	return result, nil
 }
 
 // DB에 있는 결과를 조회
